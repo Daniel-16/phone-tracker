e32345b6977b7c8ceb0381daf90c54c9a9fe2718
Daniel Oloruntoba <danieloloruntoba681@gmail.com>
2022-10-30 22:57:43 +0100

    Removed connection secrets

--- index.js
+++ index.js
@@ -7,13 +7,10 @@ const cors = require("cors");
 app.use(cors());
 
 mongoose
-  .connect(
-    "mongodb+srv://Daniel:Oloruntoba123@cluster0.ws6qroq.mongodb.net/?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     app.listen(process.env.PORT || 6000, () => {
       console.log(`Server started at port ${process.env.PORT || 6000}`);
